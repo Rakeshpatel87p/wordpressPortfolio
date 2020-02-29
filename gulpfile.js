270bbfc1cb9dfcd05e89d77e496c5e04d4d4df4d
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-02-29 00:25:36 -0500

    setting up and testing sassLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,9 @@
 const { src, dest, parallel, watch } = require("gulp");
 const server = require("gulp-webserver");
 const sass = require("gulp-sass");
+const sassLint = require("gulp-sass-lint");
 const concat = require("gulp-concat");
-
-function css() {
-  return src("scss/*.scss")
-    .pipe(sass())
-    .pipe(concat("main.css"))
-    .pipe(dest("css/"));
-}
+const log = require("fancy-log");
 
 function liveServer() {
   return src("./").pipe(
@@ -20,8 +15,38 @@ function liveServer() {
   );
 }
 
+function lintSass() {
+  return src("scss/*.scss")
+    .pipe(
+      sassLint({
+        configFile: "scss/.sasslintrc"
+      })
+    )
+    .on("error", swallowError)
+    .pipe(sassLint.format());
+}
+
+function swallowError(error) {
+  log.info("Error encountered");
+  log.error(error);
+  this.emit("end");
+}
+
+function css() {
+  return src("scss/*.scss")
+    .pipe(
+      sass({
+        errLogToConsole: true
+      })
+    )
+    .pipe(concat("main.css"))
+    .pipe(dest("css/"));
+}
+
+exports.lintSass = lintSass;
+
 exports.default = function() {
-  watch("scss/*.scss", css);
+  watch("scss/*.scss", lintSass, css);
   liveServer();
   parallel(css);
 };
