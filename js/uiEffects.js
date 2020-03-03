4a8273c1d9c2c7e5c6923d613535a671c975af77
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-03-02 22:21:11 -0500

    add new slider effect

--- js/uiEffects.js
+++ js/uiEffects.js
@@ -1,4 +1,7 @@
-headerNavDisplay();
+window.addEventListener("load", () => {
+  headerNavDisplay();
+  new PillSlider();
+});
 
 function headerNavDisplay() {
   const topPosName = document
@@ -13,4 +16,31 @@ function headerNavDisplay() {
       header.classList.remove("display");
     }
   });
+}
+
+class PillSlider {
+  constructor() {
+    this.slider = document.querySelector(".slider");
+    this.defaultSliderPos();
+    this.addClickEvents();
+  }
+
+  defaultSliderPos() {
+    const defaultBtn = document.querySelector(".controls button");
+    const leftPos = defaultBtn.offsetLeft;
+    this.moveSlider(leftPos);
+  }
+
+  addClickEvents() {
+    document.querySelectorAll(".controls button").forEach(btn => {
+      btn.addEventListener("click", () => {
+        const leftPos = btn.offsetLeft;
+        this.moveSlider(leftPos);
+      });
+    });
+  }
+
+  moveSlider(pos) {
+    this.slider.style.left = `${pos}px`;
+  }
 }
